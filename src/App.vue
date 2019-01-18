06ad1adc2f15b669ded085fa5e778d2da55b69e2
Patrick Altman <paltman@gmail.com>
2019-01-18 16:24:59 -0600

    Prototype of annotationing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="drawing = !drawing">{{ drawing ? "Stop Drawing" : "Start Drawing" }}</button>
+    <Annotator :drawing="drawing" @draw-end="drawfinish" @draw-cancel="drawcancel" @draw="onDraw">
+       <img alt="old manuscript" src="./assets/747-at150.gif">
+       <!-- <polygon class="stroke" slot="drawing" /> not supported yet -->
+      <rect class="stroke" slot="drawing" />
+    </Annotator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Annotator from 'vue-annotator';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      drawing: false
+    }
+  },
   components: {
-    HelloWorld
+    Annotator
+  },
+  methods: {
+    drawfinish (element) {
+      console.log(`finish drawing`, `<${element.type}>`, 'sdsd')
+    },
+    drawcancel () {
+      console.log('cancel drawing')
+    },
+    onDraw (element) {
+      console.log(element.node)
+    }
   }
 }
 </script>
@@ -24,5 +44,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+svg {
+  background: whitesmoke;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+.stroke {
+  stroke: limegreen;
+  stroke-width: 3px;
+}
+.stroke:hover {
+  stroke: orange;
+  stroke-width: 3px;
 }
 </style>
